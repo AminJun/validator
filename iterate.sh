206742d9343a29f26a22ef797b4892a101101cda
root <root@localhost.localdomain>
2019-03-12 05:59:44 -0400

    Added the TA name to the xls file

--- iterate.sh
+++ iterate.sh
@@ -4,8 +4,8 @@ if [ "$#" -ge 1 ] ; then
 	ta_dir=$1
 fi
 for student in `ls ${ta_dir}`; do 
-	echo ${student}
 	find "${ta_dir}${student}" -name "*.html"| while read html ; do 
-		source html.sh "${html}"
+		result=$(source html.sh "${html}")
+		echo -e "${student}\t${result}"
 	done;
 done;
