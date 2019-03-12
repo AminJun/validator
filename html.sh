2e0132aa2a33a7c54b329a8c3cf321ed1aa4f221
root <root@localhost.localdomain>
2019-03-12 05:36:00 -0400

    Uskerim

---
+++ html.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+header=$(sed "s/'/\\\\'/g; s/\"/\\\\\"/g;" $1 | tr '\n' " ")
+command=`cat << EOF 
+curl -i -s -k  -X 'POST' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0' -H 'Content-Type: multipart/form-data; boundary=---------------------------404198263875661055448819939' -H 'Referer: https://validator.w3.org/' -H 'Upgrade-Insecure-Requests: 1' --data-binary $'-----------------------------404198263875661055448819939\x0d\x0aContent-Disposition: form-data; name=\"uploaded_file\"; filename=\" \"\x0d\x0aContent-Type: text/html\x0d\x0a\x0d\x0a ${header} \x0a\x0d\x0a-----------------------------404198263875661055448819939\x0d\x0aContent-Disposition: form-data; name=\"charset\"\x0d\x0a\x0d\x0a(detect automatically)\x0d\x0a-----------------------------404198263875661055448819939\x0d\x0aContent-Disposition: form-data; name=\"doctype\"\x0d\x0a\x0d\x0aInline\x0d\x0a-----------------------------404198263875661055448819939\x0d\x0aContent-Disposition: form-data; name=\"group\"\x0d\x0a\x0d\x0a0\x0d\x0a-----------------------------404198263875661055448819939--\x0d\x0a' 'https://validator.w3.org/check'
+EOF`
+validated=$(eval $command)
+error=$(echo $validated | tr ' ' '\n' | grep "msg_err" | wc -l)
+warn=$(echo $validated | tr ' ' '\n' | grep "msg_warn" | wc -l)
+echo $error $warn
